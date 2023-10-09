4feafff5dc20e88ba03c9e5c45b30dfaaa922922
SIMONE\simon <simonecampisi2003@gmail.com>
2023-10-09 11:21:48 +0200

    first push

---
+++ src/main/java/com/simonecampisi/spotifyFormazione/SpotifyFormazioneApplication.java
@@ -0,0 +1,13 @@
+package com.simonecampisi.spotifyFormazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyFormazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyFormazioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simonecampisi/spotifyFormazione/SpotifyFormazioneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simonecampisi.spotifyFormazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyFormazioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
